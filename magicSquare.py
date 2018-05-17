1e56473f44b28be06ff305654302f99a3c5edfd9
vineshtv <tv.vinesh@gmail.com>
2018-05-18 09:37:17 +1000

    Some small changes

--- magicSquare.py
+++ magicSquare.py
@@ -1,13 +1,13 @@
 #!/bin/python3
 '''
-Solution for hackerrank problem - Forming a Magic Square. 
+Solution for hackerrank problem - Forming a Magic Square.
 Given a 3x3 matrix, find the cost of converting it to a magic square.
-You will be given a  matrix  of integers in the inclusive range . We can 
-convert any digit  to any other digit  in the range  at cost of . Given , 
+You will be given a  matrix  of integers in the inclusive range . We can
+convert any digit  to any other digit  in the range  at cost of . Given ,
 convert it into a magic square at minimal cost. Print this cost on a new line.
 
-Note: For this particular case I could have hardcoded all the magic squares 
-of order 3(because the question had it hardcoded as 3). But this is scalable 
+Note: For this particular case I could have hardcoded all the magic squares
+of order 3(because the question had it hardcoded as 3). But this is scalable
 for all indices(odd of course)
 '''
 
@@ -32,7 +32,7 @@ def createms(n):
                 j = 0
             if i < 0:
                 i = n - 1
-        
+
         #print(i, j)
         if m_s[i][j] != 0: # Case 2
             j -= 2
@@ -41,11 +41,11 @@ def createms(n):
         else:
             m_s[i][j] = num
             num += 1
-            
+
         # Update the indices according to case 1
         i -= 1
         j += 1
-    
+
     return m_s
 
 def calcCost(s, ms, n):
@@ -53,7 +53,7 @@ def calcCost(s, ms, n):
     for i in range(n):
         for j in range(n):
             cost += abs(s[i][j] - ms[i][j])
-            
+
     return cost
 
 # Complete the formingMagicSquare function below.
@@ -68,22 +68,22 @@ def formingMagicSquare(s):
     m6 = list(zip(*m2))
     m7 = list(zip(*m3))
     m8 = list(zip(*m4))
-    
+
     min_cost = calcCost(s, m1, 3)
     ms = [m2, m3, m4, m5, m6 ,m7 ,m8]
     for x in ms:
         calc_cost = calcCost(s, x, 3)
         if calc_cost < min_cost:
             min_cost = calc_cost
-    
+
     return(min_cost)
-    
+
 if __name__ == '__main__':
     s = []
 
     for _ in range(3):
         s.append(list(map(int, input().rstrip().split())))
-    
+
     result = formingMagicSquare(s)
     print(result)
-    
+
